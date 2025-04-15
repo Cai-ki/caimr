2f1c4e7f9ef8594d04e9a89052ef04e829af96f3
Caiki <2875028086@qq.com>
2025-04-15 22:52:03 +0800

    实现并测试fixedbuffer模块

---
+++ test/fixedbuffer_test.cpp
@@ -0,0 +1,12 @@
+#include "caimr/logger/fixedbuffer.h"
+
+#include <iostream>
+
+int main() {
+    fixedbuffer<KB_BUFFER_SIZE> buf;
+    buf.append("hello", 5);
+
+    std::cout << "buffer capacity: " << buf.cap() << " size: " << buf.size()
+              << " used: " << buf.used() << std::endl;
+    return 0;
+}
