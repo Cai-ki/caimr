cad02544839162777570da6d5bf681f11a9230f6
Caiki <2875028086@qq.com>
2025-04-19 17:44:07 +0800

    添加了constexpr优化

--- src/logger/logstream.cpp
+++ src/logger/logstream.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 
 namespace cai {
-static const char digits[] = {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0',
-                              '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+static constexpr char digits[] = {'9', '8', '7', '6', '5', '4', '3',
+                                  '2', '1', '0', '1', '2', '3', '4',
+                                  '5', '6', '7', '8', '9'};
 
 template <typename T>
 void logstream::format_integer(T num) {
