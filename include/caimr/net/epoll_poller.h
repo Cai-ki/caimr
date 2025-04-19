0e1dbe7d89646c5353a1c5502916fa54fd95ab27
Caiki <2875028086@qq.com>
2025-04-20 00:46:09 +0800

    实现epoll_poller模块

---
+++ include/caimr/net/epoll_poller.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <sys/epoll.h>
+
+#include <vector>
+
+#include "caimr/common/timestamp.h"
+#include "caimr/net/poller.h"
+
+namespace cai {
+class chan;
+
+class epoll_poller : public poller {
+   public:
+    explicit epoll_poller(eloop* loop);
+    ~epoll_poller() override;
+
+    timestamp poll(int timeout_ms, chanlist* active_chs) override;
+    void update_chan(chan* ch) override;
+    void remove_chan(chan* ch) override;
+
+   private:
+    static constexpr int INIT_EVENT_LIST_SIZE = 16;
+
+    void fill_active_chs(int num_events, chanlist* active_chs) const;
+    void update(int operation, chan* chan);
+
+    using eventlist = std::vector<epoll_event>;
+
+    int epollfd_;
+    eventlist events_;
+};
+}  // namespace cai
---
+++ src/net/epoll_poller.cpp
@@ -0,0 +1,76 @@
+#include "caimr/net/epoll_poller.h"
+
+#include <sys/epoll.h>
+#include <unistd.h>
+
+#include "caimr/logger/logging.h"
+#include "caimr/net/chan.h"
+
+namespace cai {
+constexpr int NEW = -1;
+constexpr int ADDED = 1;
+constexpr int DELETED = 2;
+
+epoll_poller::epoll_poller(eloop* loop)
+    : poller(loop),
+      epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
+      events_(INIT_EVENT_LIST_SIZE) {
+    if (epollfd_ < 0) {
+        LOG_FATALF("epoll_create error: %d", errno);
+    }
+}
+
+epoll_poller::~epoll_poller() { ::close(epollfd_); }
+
+timestamp epoll_poller::poll(int timeout_ms, chanlist* active_chs) {
+    int num_events = ::epoll_wait(epollfd_, &*events_.begin(),
+                                  static_cast<int>(events_.size()), timeout_ms);
+    timestamp now(timestamp::now());
+    if (num_events > 0) {
+        fill_active_chs(num_events, active_chs);
+        if (num_events == events_.size()) {
+            events_.resize(events_.size() * 2);
+        }
+    }
+    return now;
+}
+
+void epoll_poller::fill_active_chs(int num_events, chanlist* active_chs) const {
+    for (int i = 0; i < num_events; ++i) {
+        chan* ch = static_cast<chan*>(events_[i].data.ptr);
+        ch->set_revents(events_[i].events);
+        active_chs->push_back(ch);
+    }
+}
+
+void epoll_poller::update_chan(chan* chan) {
+    const int index = chan->index();
+    if (index == NEW || index == DELETED) {
+        int fd = chan->fd();
+        if (index == NEW) {
+            chs_[fd] = chan;
+        }
+        chan->set_index(ADDED);
+        update(EPOLL_CTL_ADD, chan);
+    } else {
+        update(EPOLL_CTL_MOD, chan);
+    }
+}
+
+void epoll_poller::remove_chan(chan* chan) {
+    int fd = chan->fd();
+    chs_.erase(fd);
+    chan->set_index(DELETED);
+    update(EPOLL_CTL_DEL, chan);
+}
+
+void epoll_poller::update(int operation, chan* ch) {
+    epoll_event event{};
+    event.events = ch->events();
+    event.data.ptr = ch;
+    int fd = ch->fd();
+    if (::epoll_ctl(epollfd_, operation, fd, &event) < 0) {
+        LOG_ERRORF("epoll_ctl op:%d fd:%d error:%d", operation, fd, errno);
+    }
+}
+}  // namespace cai
