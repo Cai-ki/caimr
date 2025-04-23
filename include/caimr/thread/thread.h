30b6d869b2f28cb5f015494d1063ad8af13a33e2
Caiki <2875028086@qq.com>
2025-04-23 22:28:55 +0800

    添加了部分注释，将信号量替换为std::promise/std::future，优化部分代码

--- include/caimr/thread/thread.h
+++ include/caimr/thread/thread.h
@@ -11,17 +11,23 @@
 namespace cai {
 class thread : noncopyable {
    public:
+    // 线程运行的函数
     using tfunc = std::function<void()>;
     explicit thread(tfunc func, const std::string& name = {});
     ~thread();
 
+    // 启动线程
     void start();
+    // 阻塞等待线程结束
     void join();
+    // 是否启动
     bool started() const { return started_; }
+    // 获得当前线程 id
     pid_t tid() const { return tid_; }
     const std::string& name() const { return name_; }
 
    private:
+    // 默认 name 设置
     void set_default();
     bool started_;
     bool joined_;
@@ -29,6 +35,7 @@ class thread : noncopyable {
     pid_t tid_;
     tfunc func_;
     std::string name_;
+    // 类静态原子变量，用于记录线程创建数量
     static std::atomic_int tcount_;
 };
 }  // namespace cai
--- src/thread/thread.cpp
+++ src/thread/thread.cpp
@@ -2,6 +2,9 @@
 
 #include <semaphore.h>
 
+#include <future>
+#include <memory>
+
 namespace cai {
 std::atomic_int thread::tcount_(0);
 
@@ -15,24 +18,27 @@ thread::thread(tfunc func, const std::string& name)
 }
 
 thread::~thread() {
-    if (started_ && !joined_) thread_->detach();
+    if (started_ && !joined_)
+        thread_->detach();  // 分离线程，后台运行，分离后无法再等待
 }
 
 void thread::start() {
     started_ = true;
-    sem_t sem;
-    sem_init(&sem, false, 0);
-    thread_ = std::make_shared<std::thread>([&]() {
+    std::promise<void> promise;
+    std::future<void> future = promise.get_future();
+    // std::promise 和 std::future 属于 C++ 标准库 ，定义在头文件 <future>
+    // 中。它们是 C++11 引入的线程同步工具，用于在不同线程之间传递数据或信号 。
+    thread_ = std::make_unique<std::thread>([&]() {
         tid_ = thread::tid();
-        sem_post(&sem);
+        promise.set_value();
         func_();
     });
-    sem_wait(&sem);
+    future.wait();
 }
 
 void thread::join() {
     joined_ = true;
-    thread_->join();
+    thread_->join();  // 阻塞等待线程结束，调用后线程不可再操作
 }
 
 void thread::set_default() {
