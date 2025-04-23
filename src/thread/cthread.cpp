b5a8f580aef75d3530e639c098154bdc8f5f41d7
Caiki <2875028086@qq.com>
2025-04-23 21:23:37 +0800

    添加注释并优化部分代码

--- src/thread/cthread.cpp
+++ src/thread/cthread.cpp
@@ -1,15 +1,11 @@
 #include "caimr/thread/cthread.h"
 
-#include "caimr/thread/thread.h"
-
 namespace cai {
 namespace cthread {
 thread_local int tid_ = 0;
-
-void cache() {
-    if (tid_ == 0) {
-        tid_ = static_cast<pid_t>(::syscall(SYS_gettid));
-    }
-}
+// extern thread_local int tid_
+// 如果在头文件中直接定义变量（非extern），多个源文件包含该头文件会导致多重定义错误
+// 。 C/C++ 要求全局变量（非inline 或 static）在所有编译单元中只能有一个定义
+// （One Definition Rule，ODR）。
 }  // namespace cthread
 }  // namespace cai
