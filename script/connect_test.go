a3d6b2bce98763aec835811d6567fb54ac87848b
Caiki <2875028086@qq.com>
2025-04-27 10:31:15 +0800

    添加了go语言测试脚本

---
+++ script/connect_test.go
@@ -0,0 +1,35 @@
+package script
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestEasyConnect(t *testing.T) {
+	conn, err := net.Dial("tcp", "localhost:8888")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	read := sync.OnceFunc(func() {
+		go func() {
+			for {
+				buffer := make([]byte, 1024)
+				n, err := conn.Read(buffer)
+				log.Println("receive", n, err, string(buffer[:n]))
+			}
+		}()
+	})
+	cnt := 0
+	for {
+		cnt++
+		n, err := conn.Write([]byte("hello, world!" + fmt.Sprintf(" %d", cnt)))
+		log.Println("send:", n, err)
+		read()
+		time.Sleep(1 * time.Millisecond)
+	}
+}
