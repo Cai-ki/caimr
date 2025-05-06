1127d616760aa2635fdaa3da7685c86a0382d56e
Caiki <2875028086@qq.com>
2025-05-06 21:09:01 +0800

    添加了tcp_server模块的注释，修改部分代码

--- build/caimr/net/tcp_server.h
+++ build/caimr/net/tcp_server.h
@@ -21,7 +21,7 @@ class tcp_server : noncopyable {
    public:
     using thread_init_callback = std::function<void(eloop*)>;
 
-    enum option {
+    enum option {  // 是否允许端口复用，枚举类型。
         NO_REUSE_PORT,
         REUSE_PORT,
     };
@@ -30,6 +30,7 @@ class tcp_server : noncopyable {
                option opt = NO_REUSE_PORT);
     ~tcp_server();
 
+    // 线程初始化回调
     void set_thread_init_callback(const thread_init_callback& cb) {
         thread_init_callback_ = cb;
     }
@@ -39,12 +40,16 @@ class tcp_server : noncopyable {
         write_complete_callback_ = cb;
     }
 
+    // 设置线程池大小
     void set_thread_num(int num_threads);
     void start();
 
    private:
+    // 新建连接的回调
     void new_conn(int sockfd, const address& peer_addr);
+    // 删除连接（安全的，可以多线程使用的）
     void remove_conn(const tcp_conn_ptr& conn);
+    // 删除连接的具体实现（必须传入具体对象所属线程执行，也就是调用run_in_loop()。这样才能保证并发安全。）
     void remove_conn_in_loop(const tcp_conn_ptr& conn);
 
     using conn_map = std::unordered_map<uint32_t, tcp_conn_ptr>;
@@ -53,13 +58,18 @@ class tcp_server : noncopyable {
     const std::string ip_port_;
     const std::string name_;
     std::unique_ptr<acceptor> acceptor_;
-    std::shared_ptr<eloop_tpool> thread_pool_;
+    std::unique_ptr<eloop_tpool> thread_pool_;
+
     close_callback conn_callback_;
     msg_callback msg_callback_;
     write_complete_callback write_complete_callback_;
     thread_init_callback thread_init_callback_;
+
+    // 启动次数
     std::atomic_int started_;
+    // 下一个新连接id
     uint32_t next_conn_id_;
+    // 维护已有连接
     conn_map conns_;
 };
 }  // namespace cai
--- src/net/tcp_server.cpp
+++ src/net/tcp_server.cpp
@@ -8,7 +8,7 @@
 #include "net/eloop_tpool.h"
 
 namespace cai {
-namespace {
+namespace {  // 匿名namespace中的变量和对象的生命周期与当前翻译单元绑定，确保仅当前文件可见可以避免命名冲突。
 eloop* check_loop_not_null(eloop* loop) {
     if (!loop) {
         LOG_FATALF("main event loop is null");
@@ -26,9 +26,9 @@ tcp_server::tcp_server(eloop* loop, const address& listen_addr,
       thread_pool_(new eloop_tpool(loop, name)),
       next_conn_id_(1),
       started_(0) {
-    acceptor_->set_new_conn_callback(std::bind(&tcp_server::new_conn, this,
-                                               std::placeholders::_1,
-                                               std::placeholders::_2));
+    acceptor_->set_new_conn_callback(
+        std::bind(&tcp_server::new_conn, this, std::placeholders::_1,
+                  std::placeholders::_2));  // 设置回调
 }
 
 tcp_server::~tcp_server() {
@@ -47,12 +47,15 @@ void tcp_server::set_thread_num(int num_threads) {
 void tcp_server::start() {
     if (started_++ == 0) {
         thread_pool_->start(thread_init_callback_);
-        loop_->run_in_loop(std::bind(&acceptor::listen, acceptor_.get()));
+        loop_->run_in_loop(std::bind(&acceptor::listen,
+                                     acceptor_.get()));  // 主线程执行监听逻辑
     }
 }
 
 void tcp_server::new_conn(int sockfd, const address& peer_addr) {
-    eloop* io_loop = thread_pool_->get_next_loop();
+    eloop* io_loop =
+        thread_pool_
+            ->get_next_loop();  // 每个连接挨着选择eloop便于将任务均匀分配给每个线程
     char buf[64];
     snprintf(buf, sizeof buf, "-%s#%d", ip_port_.c_str(), next_conn_id_);
     std::string conn_name = name_ + buf;
@@ -73,13 +76,18 @@ void tcp_server::new_conn(int sockfd, const address& peer_addr) {
                                    local_addr, peer_addr));
 
     conns_[next_conn_id_++] = conn;
+
+    // 设置回调
     conn->set_conn_callback(conn_callback_);
     conn->set_msg_callback(msg_callback_);
     conn->set_write_complete_callback(write_complete_callback_);
     conn->set_close_callback(
         std::bind(&tcp_server::remove_conn, this, std::placeholders::_1));
 
-    io_loop->run_in_loop(std::bind(&tcp_conn::establish_conn, conn));
+    // conn所属线程执行新连接建立的回调
+    io_loop->run_in_loop(std::bind(
+        &tcp_conn::establish_conn,
+        conn));  // tcp_conn::establish_conn的ch_->enable_reading()会向poller注册连接的读事件，至此新数据来时就会自动触发回调。而发送事件则是当我们第一次send并且数据一下子并没有发送完时执行注册。
 }
 
 void tcp_server::remove_conn(const tcp_conn_ptr& conn) {
